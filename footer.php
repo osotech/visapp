03dc521841121ecfa0ef76119364015da7299802
Flourish Samson <64082757+osotech@users.noreply.github.com>
2021-06-29 19:51:24 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,104 @@
+  <footer>
+      <div class="footer-top">
+          <div class="pt-exebar">
+              <div class="container">
+                  <div class="d-flex align-items-stretch">
+                      <div class="pt-logo mr-auto">
+                          <a href="index?p=home_"><img src="assets/images/logo-4.png" width="100%" alt="logo" /></a>
+                      </div>
+                      <div class="pt-social-link">
+                          <ul class="list-inline m-a0">
+                              <li><a href="#" class="btn-link"><i class="fa fa-facebook"></i></a></li>
+                              <li><a href="#" class="btn-link"><i class="fa fa-twitter"></i></a></li>
+                              <li><a href="#" class="btn-link"><i class="fa fa-linkedin"></i></a></li>
+                              <li><a href="#" class="btn-link"><i class="fa fa-google-plus"></i></a></li>
+                          </ul>
+                      </div>
+                      <div class="pt-btn-join">
+                          <a href="../oso/e-portal/index" class="btn">Start your Admission Now</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-4 col-md-12 col-sm-12 footer-col-4">
+                      <div class="widget">
+                          <h5 class="footer-title">Sign Up For A Newsletter</h5>
+                          <p class="text-capitalize m-b20">Weekly Breaking news analysis and cutting edge advices on job
+                              searching.</p>
+                          <div class="subscribe-form m-b20">
+                              <form class="subscription-form" action="" method="post">
+                                  <div class="ajax-message"></div>
+                                  <div class="input-group">
+                                      <input name="email" required="required" class="form-control"
+                                          placeholder="Your Email Address" type="email">
+                                      <span class="input-group-btn">
+                                          <button name="submit" value="Submit" type="submit" class="btn"><i
+                                                  class="fa fa-arrow-right"></i></button>
+                                      </span>
+                                  </div>
+                              </form>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-12 col-lg-5 col-md-7 col-sm-12">
+                      <div class="row">
+                          <div class="col-4 col-lg-4 col-md-4 col-sm-4">
+                              <div class="widget footer_widget">
+                                  <h5 class="footer-title">Company</h5>
+                                  <ul>
+                                      <li><a href="index?p=home_">Home</a></li>
+                                      <li><a href="about?p=about_">About</a></li>
+                                      <li><a href="faq?p=faq_">FAQs</a></li>
+                                      <li><a href="contact?p=contact_">Contact</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                          <div class="col-4 col-lg-4 col-md-4 col-sm-4">
+                              <div class="widget footer_widget">
+                                  <h5 class="footer-title">Get In Touch</h5>
+                                  <ul>
+                                      <li><a href="facilities.php">Facilities</a></li>
+                                      <li><a href="blog?p=blog_">Blog</a></li>
+                                      <li><a href="fee-structure?p=fee_">Fee Structure</a></li>
+                                      <li><a href="event?p=event_">Event</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                          <div class="col-4 col-lg-4 col-md-4 col-sm-4">
+                              <div class="widget footer_widget">
+                                  <h5 class="footer-title">Portal</h5>
+                                  <ul>
+                                      <li><a href="#">Staff</a></li>
+                                      <li><a href="#">Student</a></li>
+                                      <li><a href="#">e-Registration</a></li>
+                                      <li><a href="#">CBT</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-12 col-lg-3 col-md-5 col-sm-12 footer-col-4">
+                      <div class="widget widget_gallery gallery-grid-4">
+                          <h5 class="footer-title">Our Gallery</h5>
+                          <ul class="magnific-image">
+                              <li>
+                                  <a href="assets/images/gallery/pic1.jpg" class="magnific-anchor"><img
+                                          src="assets/images/gallery/pic1.jpg" alt=""></a>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="footer-bottom">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 text-center"> <a target="_blank"
+                          href="https://www.ikraamidealmodelcollege.com">Ikraamidealmodelcollege</a></div>
+              </div>
+          </div>
+      </div>
+  </footer>
